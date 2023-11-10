c1d903851ec0a68bd3a2e0ecb1ee74391dbc7d21
june-777 <wlwhswnsrl96@gmail.com>
2023-11-10 18:08:00 +0900

    feat(OrderMenu): 주문 메뉴(메뉴 이름, 주문 개수)를 담고 있는 DTO 구현

---
+++ src/main/java/christmas/dto/OrderMenu.java
@@ -0,0 +1,19 @@
+package christmas.dto;
+
+public class OrderMenu {
+    private final String menuName;
+    private final int orderCount;
+
+    public OrderMenu(String menuName, int orderCount) {
+        this.menuName = menuName;
+        this.orderCount = orderCount;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public int getOrderCount() {
+        return orderCount;
+    }
+}
