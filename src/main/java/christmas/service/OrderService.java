8acaccb80c464f341c272af4a9df690145e336dd
june-777 <wlwhswnsrl96@gmail.com>
2023-11-13 21:53:19 +0900

    docs(OrderService): createOrder 메서드 시그니쳐 변경
    [변경대상]
    1. 파라미터를 List<OrderItemRequest>에서 OrderRequest로 변경

--- src/main/java/christmas/service/OrderService.java
+++ src/main/java/christmas/service/OrderService.java
@@ -3,14 +3,15 @@ package christmas.service;
 import christmas.domain.order.Order;
 import christmas.domain.order.OrderItem;
 import christmas.dto.OrderItemRequest;
+import christmas.dto.OrderRequest;
 import java.util.ArrayList;
 import java.util.List;
 
 public class OrderService {
 
-    public Order createOrder(List<OrderItemRequest> orderItemRequests) {
-        List<OrderItem> orderItem = createOrderItems(orderItemRequests);
-        return new Order(orderItem);
+    public Order createOrder(OrderRequest orderRequest) {
+        List<OrderItem> orderItem = createOrderItems(orderRequest.getOrderItems());
+        return new Order(orderItem, orderRequest.getOrderDay());
     }
 
     private List<OrderItem> createOrderItems(List<OrderItemRequest> orderItemRequests) {
