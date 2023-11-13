906535d8dfa76e6acdc7795c9a03a676ea35a1c1
june-777 <wlwhswnsrl96@gmail.com>
2023-11-13 14:20:16 +0900

    feat(OrderService): 주문 아이템 리스트를 생성하는 기능

--- src/main/java/christmas/service/OrderService.java
+++ src/main/java/christmas/service/OrderService.java
@@ -3,6 +3,7 @@ package christmas.service;
 import christmas.domain.order.Order;
 import christmas.domain.order.OrderItem;
 import christmas.dto.OrderItemRequest;
+import java.util.ArrayList;
 import java.util.List;
 
 public class OrderService {
@@ -10,6 +11,15 @@ public class OrderService {
     public Order createOrder(List<OrderItemRequest> orderItemRequests) {
         List<OrderItem> orderItems = createOrderItems(orderItemRequests);
         return new Order(orderItems);
+    }
+
+    private List<OrderItem> createOrderItems(List<OrderItemRequest> orderItemRequests) {
+        List<OrderItem> orderItems = new ArrayList<>();
+        for (OrderItemRequest orderItemRequest : orderItemRequests) {
+            OrderItem orderItem = new OrderItem(orderItemRequest.getMenuName(), orderItemRequest.getOrderCount());
+            orderItems.add(orderItem);
+        }
+        return orderItems;
     }
 }
 
