6aea119e863409aa49262a9d0a4f18b1b6f25983
june-777 <wlwhswnsrl96@gmail.com>
2023-11-13 21:44:21 +0900

    feat(OrderDay): 주문 날짜를 의미하는 클래스, 1 ~ 31일인지 검증하는 기능

---
+++ src/main/java/christmas/domain/order/OrderDay.java
@@ -0,0 +1,23 @@
+package christmas.domain.order;
+
+import christmas.domain.exception.OutOfRangeDayException;
+
+public class OrderDay {
+    private static final int MIN_DAY = 1;
+    private static final int MAX_DAY = 31;
+    private final int day;
+
+    public OrderDay(int day) {
+        this.day = day;
+    }
+
+    private void validate(int day) {
+        if (day < MIN_DAY || day > MAX_DAY) {
+            throw new OutOfRangeDayException();
+        }
+    }
+
+    public int getDay() {
+        return day;
+    }
+}
