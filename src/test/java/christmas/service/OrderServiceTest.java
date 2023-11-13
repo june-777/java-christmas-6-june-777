2bd505d2f9db80db9f84531deebf180845170611
june-777 <wlwhswnsrl96@gmail.com>
2023-11-13 21:56:10 +0900

    test(OrderServiceTest): createOrder의 파라미터 변경 및 테스트 데이터 클래스 이름 변경으로 인한 테스트 로직 수정 및 재테스트

--- src/test/java/christmas/service/OrderServiceTest.java
+++ src/test/java/christmas/service/OrderServiceTest.java
@@ -5,8 +5,7 @@ import christmas.domain.exception.ExceedTotalMenuCountException;
 import christmas.domain.exception.MenuNotFoundException;
 import christmas.domain.exception.OnlyBeverageMenuException;
 import christmas.domain.exception.OutOfRangeOrderItemCountException;
-import christmas.dto.OrderItemRequest;
-import java.util.List;
+import christmas.dto.OrderRequest;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -15,7 +14,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 class OrderServiceTest {
 
-    private static final String ORDER_ITEM_REQUEST_GENERATOR_PATH = "christmas.helper.OrderItemRequestGenerator";
+    private static final String ORDER_REQUEST_GENERATOR_PATH = "christmas.helper.OrderRequestGenerator";
     OrderService orderService = new OrderService();
 
     @Nested
@@ -23,97 +22,97 @@ class OrderServiceTest {
     class FailOrderTest {
 
         @ParameterizedTest
-        @MethodSource(ORDER_ITEM_REQUEST_GENERATOR_PATH + "#createOrderNotExistMenu")
+        @MethodSource(ORDER_REQUEST_GENERATOR_PATH + "#createOrderNotExistMenu")
         @DisplayName("[Exception] 존재하지 않는 메뉴이면 -> MenuNotFoundException이 발생한다.")
-        void notExistMenuMany(List<OrderItemRequest> orderMenus) {
+        void notExistMenuMany(OrderRequest orderMenus) {
             Assertions.assertThatThrownBy(() -> orderService.createOrder(orderMenus))
                     .isInstanceOf(MenuNotFoundException.class);
         }
 
         @ParameterizedTest
-        @MethodSource(ORDER_ITEM_REQUEST_GENERATOR_PATH + "#createNotExistAndDuplicateMenu")
+        @MethodSource(ORDER_REQUEST_GENERATOR_PATH + "#createNotExistAndDuplicateMenu")
         @DisplayName("[Exception] 존재하지 않는 메뉴 + 중복 메뉴이면 -> MenuNotFoundException이 발생한다.")
-        void notExistMenuAndDuplicate(List<OrderItemRequest> orderMenus) {
+        void notExistMenuAndDuplicate(OrderRequest orderMenus) {
             Assertions.assertThatThrownBy(() -> orderService.createOrder(orderMenus))
                     .isInstanceOf(MenuNotFoundException.class);
         }
 
         @ParameterizedTest
-        @MethodSource(ORDER_ITEM_REQUEST_GENERATOR_PATH + "#createNotExistAndOnlyBeverageMenu")
+        @MethodSource(ORDER_REQUEST_GENERATOR_PATH + "#createNotExistAndOnlyBeverageMenu")
         @DisplayName("[Exception] 존재하지 않는 메뉴 + 음료수만 주문이면 -> MenuNotFoundException이 발생한다.")
-        void notExistAndOnlyBeverageMenu(List<OrderItemRequest> orderMenus) {
+        void notExistAndOnlyBeverageMenu(OrderRequest orderMenus) {
             Assertions.assertThatThrownBy(() -> orderService.createOrder(orderMenus))
                     .isInstanceOf(MenuNotFoundException.class);
         }
 
         @ParameterizedTest
-        @MethodSource(ORDER_ITEM_REQUEST_GENERATOR_PATH + "#createNotExistAndExceedCount")
+        @MethodSource(ORDER_REQUEST_GENERATOR_PATH + "#createNotExistAndExceedCount")
         @DisplayName("[Exception] 존재하지 않는 메뉴 + 주문 개수 초과이면 -> MenuNotFoundException이 발생한다.")
-        void notExistAndExceedMenuCount(List<OrderItemRequest> orderMenus) {
+        void notExistAndExceedMenuCount(OrderRequest orderMenus) {
             Assertions.assertThatThrownBy(() -> orderService.createOrder(orderMenus))
                     .isInstanceOf(MenuNotFoundException.class);
         }
 
         @ParameterizedTest
-        @MethodSource(ORDER_ITEM_REQUEST_GENERATOR_PATH + "#createNotExistAndMix")
+        @MethodSource(ORDER_REQUEST_GENERATOR_PATH + "#createNotExistAndMix")
         @DisplayName("[Exception] 존재하지 않는 메뉴 + 중복 메뉴 + 음료수만 주문 + 주문 개수 초과이면 -> MenuNotFoundException이 발생한다.")
-        void notExistTotalMix(List<OrderItemRequest> orderMenus) {
+        void notExistTotalMix(OrderRequest orderMenus) {
             Assertions.assertThatThrownBy(() -> orderService.createOrder(orderMenus))
                     .isInstanceOf(MenuNotFoundException.class);
         }
 
         @ParameterizedTest
-        @MethodSource(ORDER_ITEM_REQUEST_GENERATOR_PATH + "#createOutOfRangeLessMin")
+        @MethodSource(ORDER_REQUEST_GENERATOR_PATH + "#createOutOfRangeLessMin")
         @DisplayName("[Exception] 단 건 주문 상품 개수가 1개 미만이면 -> OutOfRangeOrderItemCountException 발생한다.")
-        void outOfRangeLessMin(List<OrderItemRequest> orderMenus) {
+        void outOfRangeLessMin(OrderRequest orderMenus) {
             Assertions.assertThatThrownBy(() -> orderService.createOrder(orderMenus))
                     .isInstanceOf(OutOfRangeOrderItemCountException.class);
         }
 
         @ParameterizedTest
-        @MethodSource(ORDER_ITEM_REQUEST_GENERATOR_PATH + "#createOutOfRangeLessMinAndDuplicate")
+        @MethodSource(ORDER_REQUEST_GENERATOR_PATH + "#createOutOfRangeLessMinAndDuplicate")
         @DisplayName("[Exception] 단 건 주문 상품 개수가 1개 미만 + 중복 메뉴이면 -> OutOfRangeOrderItemCountException 발생한다.")
-        void outOfRangeLessMinAndDuplicate(List<OrderItemRequest> orderMenus) {
+        void outOfRangeLessMinAndDuplicate(OrderRequest orderMenus) {
             Assertions.assertThatThrownBy(() -> orderService.createOrder(orderMenus))
                     .isInstanceOf(OutOfRangeOrderItemCountException.class);
         }
 
         @ParameterizedTest
-        @MethodSource(ORDER_ITEM_REQUEST_GENERATOR_PATH + "#createOutOfRangeGreaterMax")
+        @MethodSource(ORDER_REQUEST_GENERATOR_PATH + "#createOutOfRangeGreaterMax")
         @DisplayName("[Exception] 단 건 주문 상품 개수가 20개 초과이면 -> OutOfRangeOrderItemCountException 발생한다.")
-        void outOfRangeGreaterMax(List<OrderItemRequest> orderMenus) {
+        void outOfRangeGreaterMax(OrderRequest orderMenus) {
             Assertions.assertThatThrownBy(() -> orderService.createOrder(orderMenus))
                     .isInstanceOf(OutOfRangeOrderItemCountException.class);
         }
 
         @ParameterizedTest
-        @MethodSource(ORDER_ITEM_REQUEST_GENERATOR_PATH + "#createOutOfRangeGreaterMaxAndOnlyBeverage")
+        @MethodSource(ORDER_REQUEST_GENERATOR_PATH + "#createOutOfRangeGreaterMaxAndOnlyBeverage")
         @DisplayName("[Exception] 단 건 주문 상품 개수가 20개 초과 + 음료수만 주문이면 -> OutOfRangeOrderItemCountException 발생한다.")
-        void outOfRangeGreaterMaxAndOnlyBeverage(List<OrderItemRequest> orderMenus) {
+        void outOfRangeGreaterMaxAndOnlyBeverage(OrderRequest orderMenus) {
             Assertions.assertThatThrownBy(() -> orderService.createOrder(orderMenus))
                     .isInstanceOf(OutOfRangeOrderItemCountException.class);
         }
 
         @ParameterizedTest
-        @MethodSource(ORDER_ITEM_REQUEST_GENERATOR_PATH + "#createDuplicate")
+        @MethodSource(ORDER_REQUEST_GENERATOR_PATH + "#createDuplicate")
         @DisplayName("[Exception] 중복 메뉴 주문이면 -> DuplicateMenuException이 발생한다.")
-        void duplicate(List<OrderItemRequest> orderMenus) {
+        void duplicate(OrderRequest orderMenus) {
             Assertions.assertThatThrownBy(() -> orderService.createOrder(orderMenus))
                     .isInstanceOf(DuplicateMenuException.class);
         }
 
         @ParameterizedTest
-        @MethodSource(ORDER_ITEM_REQUEST_GENERATOR_PATH + "#createOnlyBeverage")
+        @MethodSource(ORDER_REQUEST_GENERATOR_PATH + "#createOnlyBeverage")
         @DisplayName("[Exception] 음료수 메뉴만 주문이면 -> OnlyBeverageMenuException이 발생한다.")
-        void onlyBeverage(List<OrderItemRequest> orderMenus) {
+        void onlyBeverage(OrderRequest orderMenus) {
             Assertions.assertThatThrownBy(() -> orderService.createOrder(orderMenus))
                     .isInstanceOf(OnlyBeverageMenuException.class);
         }
 
         @ParameterizedTest
-        @MethodSource(ORDER_ITEM_REQUEST_GENERATOR_PATH + "#createExceedTotalOrderCount")
+        @MethodSource(ORDER_REQUEST_GENERATOR_PATH + "#createExceedTotalOrderCount")
         @DisplayName("[Exception] 총 주문 개수가 20개 초과이면 -> ExceedTotalMenuCountException 발생한다.")
-        void exceedTotalOrderCount(List<OrderItemRequest> orderMenus) {
+        void exceedTotalOrderCount(OrderRequest orderMenus) {
             Assertions.assertThatThrownBy(() -> orderService.createOrder(orderMenus))
                     .isInstanceOf(ExceedTotalMenuCountException.class);
         }
@@ -123,9 +122,9 @@ class OrderServiceTest {
     @DisplayName("주문 성공 테스트")
     class SuccessOrderTest {
         @ParameterizedTest
-        @MethodSource(ORDER_ITEM_REQUEST_GENERATOR_PATH + "#createSuccess")
+        @MethodSource(ORDER_REQUEST_GENERATOR_PATH + "#createSuccess")
         @DisplayName("[Success] 올바른 주문이면 -> 예외가 발생하지 않는다.")
-        void exceedTotalOrderCount(List<OrderItemRequest> orderMenus) {
+        void exceedTotalOrderCount(OrderRequest orderMenus) {
             Assertions.assertThatCode(() -> orderService.createOrder(orderMenus))
                     .doesNotThrowAnyException();
         }
