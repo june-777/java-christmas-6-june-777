e2ddce6fa967dcf0fde367927bc22ec7048788fa
june-777 <wlwhswnsrl96@gmail.com>
2023-11-13 21:54:54 +0900

    refactor(test/OrderRequestGenerator): 테스트용 DTO 데이터를 OrderRequest로 생성하도록 변경 및 클래스 이름 변경

--- src/test/java/christmas/helper/OrderRequestGenerator.java
+++ src/test/java/christmas/helper/OrderRequestGenerator.java
@@ -1,11 +1,12 @@
 package christmas.helper;
 
 import christmas.dto.OrderItemRequest;
+import christmas.dto.OrderRequest;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class OrderItemRequestGenerator {
-    public static Stream<List<OrderItemRequest>> createOrderNotExistMenu() {
+public class OrderRequestGenerator {
+    public static Stream<OrderRequest> createOrderNotExistMenu() {
         List<OrderItemRequest> orderItems = List.of(
                 new OrderItemRequest("", 1),
                 new OrderItemRequest(" ", 2),
@@ -22,30 +23,32 @@ public class OrderItemRequestGenerator {
                 new OrderItemRequest("제로콜ㄹ", 8),
                 new OrderItemRequest("레드와인!", 9),
                 new OrderItemRequest("샴페인\n", 11));
-
-        return Stream.of(orderItems);
+        OrderRequest orderRequest = new OrderRequest(orderItems, 1);
+        return Stream.of(orderRequest);
     }
 
-    public static Stream<List<OrderItemRequest>> createNotExistAndDuplicateMenu() {
+    public static Stream<OrderRequest> createNotExistAndDuplicateMenu() {
         List<OrderItemRequest> orderItems = List.of(
                 new OrderItemRequest("제로콜라", 1),
                 new OrderItemRequest("크리스마스파스타", 2),
                 new OrderItemRequest("샴페인", 3),
                 new OrderItemRequest("", 4),
                 new OrderItemRequest("크리스마스파스타", 4));
-        return Stream.of(orderItems);
+        OrderRequest orderRequest = new OrderRequest(orderItems, 31);
+        return Stream.of(orderRequest);
     }
 
-    public static Stream<List<OrderItemRequest>> createNotExistAndOnlyBeverageMenu() {
+    public static Stream<OrderRequest> createNotExistAndOnlyBeverageMenu() {
         List<OrderItemRequest> orderItems = List.of(
                 new OrderItemRequest("제로콜라", 1),
                 new OrderItemRequest("레드와인", 2),
                 new OrderItemRequest("샴페인", 3),
                 new OrderItemRequest("", 4));
-        return Stream.of(orderItems);
+        OrderRequest orderRequest = new OrderRequest(orderItems, 30);
+        return Stream.of(orderRequest);
     }
 
-    public static Stream<List<OrderItemRequest>> createNotExistAndExceedCount() {
+    public static Stream<OrderRequest> createNotExistAndExceedCount() {
         List<OrderItemRequest> orderItems = List.of(
                 new OrderItemRequest("제로콜라", 1),
                 new OrderItemRequest("레드 와인", 2),
@@ -54,30 +57,33 @@ public class OrderItemRequestGenerator {
                 new OrderItemRequest("티본스테이크", 10),
                 new OrderItemRequest("초코케이크", 3),
                 new OrderItemRequest("시저샐러드", 1));
-        return Stream.of(orderItems);
+        OrderRequest orderRequest = new OrderRequest(orderItems, 25);
+        return Stream.of(orderRequest);
     }
 
-    public static Stream<List<OrderItemRequest>> createNotExistAndMix() {
+    public static Stream<OrderRequest> createNotExistAndMix() {
         List<OrderItemRequest> orderItems = List.of(
                 new OrderItemRequest("샴페인", 1),
                 new OrderItemRequest("레드 와인", 2),
                 new OrderItemRequest("샴페인 ", 10),
                 new OrderItemRequest("바비큐립", 10),
                 new OrderItemRequest("바비큐립", 10));
-        return Stream.of(orderItems);
+        OrderRequest orderRequest = new OrderRequest(orderItems, 15);
+        return Stream.of(orderRequest);
     }
 
-    public static Stream<List<OrderItemRequest>> createOutOfRangeLessMin() {
+    public static Stream<OrderRequest> createOutOfRangeLessMin() {
         List<OrderItemRequest> orderItems = List.of(
                 new OrderItemRequest("샴페인", 1),
                 new OrderItemRequest("레드와인", 2),
                 new OrderItemRequest("해산물파스타", 3),
                 new OrderItemRequest("바비큐립", 4),
                 new OrderItemRequest("크리스마스파스타", 0));
-        return Stream.of(orderItems);
+        OrderRequest orderRequest = new OrderRequest(orderItems, 15);
+        return Stream.of(orderRequest);
     }
 
-    public static Stream<List<OrderItemRequest>> createOutOfRangeLessMinAndDuplicate() {
+    public static Stream<OrderRequest> createOutOfRangeLessMinAndDuplicate() {
         List<OrderItemRequest> orderItems = List.of(
                 new OrderItemRequest("샴페인", 1),
                 new OrderItemRequest("양송이수프", 2),
@@ -85,47 +91,52 @@ public class OrderItemRequestGenerator {
                 new OrderItemRequest("바비큐립", 4),
                 new OrderItemRequest("티본스테이크", 0),
                 new OrderItemRequest("티본스테이크", 7));
-        return Stream.of(orderItems);
+        OrderRequest orderRequest = new OrderRequest(orderItems, 15);
+        return Stream.of(orderRequest);
     }
 
-    public static Stream<List<OrderItemRequest>> createOutOfRangeGreaterMax() {
+    public static Stream<OrderRequest> createOutOfRangeGreaterMax() {
         List<OrderItemRequest> orderItems = List.of(
                 new OrderItemRequest("초코케이크", 1),
                 new OrderItemRequest("레드와인", 2),
                 new OrderItemRequest("아이스크림", 3),
                 new OrderItemRequest("바비큐립", 4),
                 new OrderItemRequest("제로콜라", 21));
-        return Stream.of(orderItems);
+        OrderRequest orderRequest = new OrderRequest(orderItems, 15);
+        return Stream.of(orderRequest);
     }
 
-    public static Stream<List<OrderItemRequest>> createOutOfRangeGreaterMaxAndOnlyBeverage() {
+    public static Stream<OrderRequest> createOutOfRangeGreaterMaxAndOnlyBeverage() {
         List<OrderItemRequest> orderItems = List.of(
                 new OrderItemRequest("레드와인", 2),
                 new OrderItemRequest("제로콜라", 3),
                 new OrderItemRequest("샴페인", 4),
                 new OrderItemRequest("제로콜라", 21));
-        return Stream.of(orderItems);
+        OrderRequest orderRequest = new OrderRequest(orderItems, 15);
+        return Stream.of(orderRequest);
     }
 
-    public static Stream<List<OrderItemRequest>> createDuplicate() {
+    public static Stream<OrderRequest> createDuplicate() {
         List<OrderItemRequest> orderItems = List.of(
                 new OrderItemRequest("초코케이크", 1),
                 new OrderItemRequest("레드와인", 2),
                 new OrderItemRequest("아이스크림", 3),
                 new OrderItemRequest("바비큐립", 4),
                 new OrderItemRequest("초코케이크", 7));
-        return Stream.of(orderItems);
+        OrderRequest orderRequest = new OrderRequest(orderItems, 15);
+        return Stream.of(orderRequest);
     }
 
-    public static Stream<List<OrderItemRequest>> createOnlyBeverage() {
+    public static Stream<OrderRequest> createOnlyBeverage() {
         List<OrderItemRequest> orderItems = List.of(
                 new OrderItemRequest("제로콜라", 1),
                 new OrderItemRequest("레드와인", 2),
                 new OrderItemRequest("샴페인", 7));
-        return Stream.of(orderItems);
+        OrderRequest orderRequest = new OrderRequest(orderItems, 15);
+        return Stream.of(orderRequest);
     }
 
-    public static Stream<List<OrderItemRequest>> createExceedTotalOrderCount() {
+    public static Stream<OrderRequest> createExceedTotalOrderCount() {
         List<OrderItemRequest> orderItems = List.of(
                 new OrderItemRequest("양송이수프", 1),
                 new OrderItemRequest("바비큐립", 2),
@@ -134,10 +145,11 @@ public class OrderItemRequestGenerator {
                 new OrderItemRequest("샴페인", 5),
                 new OrderItemRequest("크리스마스파스타", 5),
                 new OrderItemRequest("해산물파스타", 1));
-        return Stream.of(orderItems);
+        OrderRequest orderRequest = new OrderRequest(orderItems, 15);
+        return Stream.of(orderRequest);
     }
 
-    public static Stream<List<OrderItemRequest>> createSuccess() {
+    public static Stream<OrderRequest> createSuccess() {
         List<OrderItemRequest> orderItems = List.of(
                 new OrderItemRequest("양송이수프", 1),
                 new OrderItemRequest("타파스", 1),
@@ -151,6 +163,7 @@ public class OrderItemRequestGenerator {
                 new OrderItemRequest("제로콜라", 1),
                 new OrderItemRequest("레드와인", 1),
                 new OrderItemRequest("샴페인", 9));
-        return Stream.of(orderItems);
+        OrderRequest orderRequest = new OrderRequest(orderItems, 15);
+        return Stream.of(orderRequest);
     }
 }
