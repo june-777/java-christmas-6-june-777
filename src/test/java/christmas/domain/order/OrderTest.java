740cff31191d963e874cfbc7f4fcb8becf154c48
june-777 <wlwhswnsrl96@gmail.com>
2023-11-13 21:56:39 +0900

    test(OrderTest): Order 생성자의 파라미터 변경으로 인한 테스트 코드 수정 및 재 테스트

--- src/test/java/christmas/domain/order/OrderTest.java
+++ src/test/java/christmas/domain/order/OrderTest.java
@@ -1,5 +1,6 @@
 package christmas.domain.order;
 
+import christmas.domain.MenuType;
 import christmas.domain.exception.DuplicateMenuException;
 import christmas.domain.exception.ExceedTotalMenuCountException;
 import christmas.domain.exception.OnlyBeverageMenuException;
@@ -7,6 +8,7 @@ import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -22,7 +24,7 @@ class OrderTest {
         @MethodSource(ORDER_ITEMS_GENERATOR_PATH + "#createDuplicate")
         @DisplayName("[Exception] 중복 메뉴 주문이면 -> DuplicateMenuException 발생한다.")
         void duplicate(List<OrderItem> orderItems) {
-            Assertions.assertThatThrownBy(() -> new Order(orderItems))
+            Assertions.assertThatThrownBy(() -> new Order(orderItems, 1))
                     .isInstanceOf(DuplicateMenuException.class);
         }
 
@@ -30,7 +32,7 @@ class OrderTest {
         @MethodSource(ORDER_ITEMS_GENERATOR_PATH + "#createDuplicateAndOnlyBeverage")
         @DisplayName("[Exception] 중복 메뉴 + 음료수만 주문이면 -> DuplicateMenuException 발생한다.")
         void duplicateAndOnlyBeverage(List<OrderItem> orderItems) {
-            Assertions.assertThatThrownBy(() -> new Order(orderItems))
+            Assertions.assertThatThrownBy(() -> new Order(orderItems, 31))
                     .isInstanceOf(DuplicateMenuException.class);
         }
 
@@ -38,7 +40,7 @@ class OrderTest {
         @MethodSource(ORDER_ITEMS_GENERATOR_PATH + "#createDuplicateAndExceedCount")
         @DisplayName("[Exception] 중복 메뉴 + 총 주문 개수 초과 주문이면 -> DuplicateMenuException 발생한다.")
         void duplicateAndExceedCount(List<OrderItem> orderItems) {
-            Assertions.assertThatThrownBy(() -> new Order(orderItems))
+            Assertions.assertThatThrownBy(() -> new Order(orderItems, 2))
                     .isInstanceOf(DuplicateMenuException.class);
         }
 
@@ -46,7 +48,7 @@ class OrderTest {
         @MethodSource(ORDER_ITEMS_GENERATOR_PATH + "#createDuplicateAndMix")
         @DisplayName("[Exception] 중복 메뉴 + 음료수만 + 총 주문 개수 초과 주문이면 -> DuplicateMenuException 발생한다.")
         void duplicateAndMix(List<OrderItem> orderItems) {
-            Assertions.assertThatThrownBy(() -> new Order(orderItems))
+            Assertions.assertThatThrownBy(() -> new Order(orderItems, 3))
                     .isInstanceOf(DuplicateMenuException.class);
         }
     }
@@ -59,7 +61,7 @@ class OrderTest {
         @MethodSource(ORDER_ITEMS_GENERATOR_PATH + "#createOnlyBeverage")
         @DisplayName("[Exception] 음료수만 존재하면 AllMenuBeverageException이 발생한다.")
         void allMenuBeverage(List<OrderItem> orderItems) {
-            Assertions.assertThatThrownBy(() -> new Order(orderItems))
+            Assertions.assertThatThrownBy(() -> new Order(orderItems, 12))
                     .isInstanceOf(OnlyBeverageMenuException.class);
         }
 
@@ -67,7 +69,7 @@ class OrderTest {
         @MethodSource(ORDER_ITEMS_GENERATOR_PATH + "#createOnlyBeverageAndExceedCount")
         @DisplayName("[Exception] 음료수만 존재 + 총 주문 개수 초과하면 AllMenuBeverageException이 발생한다.")
         void allMenuBeverageAndExceedMenuCount(List<OrderItem> orderItems) {
-            Assertions.assertThatThrownBy(() -> new Order(orderItems))
+            Assertions.assertThatThrownBy(() -> new Order(orderItems, 21))
                     .isInstanceOf(OnlyBeverageMenuException.class);
         }
     }
@@ -79,7 +81,7 @@ class OrderTest {
         @MethodSource(ORDER_ITEMS_GENERATOR_PATH + "#createExceedCount")
         @DisplayName("[Exception] 총 주문 개수 초과하면 ExceedTotalMenuCountException 발생한다.")
         void exceedCount(List<OrderItem> orderItems) {
-            Assertions.assertThatThrownBy(() -> new Order(orderItems))
+            Assertions.assertThatThrownBy(() -> new Order(orderItems, 30))
                     .isInstanceOf(ExceedTotalMenuCountException.class);
         }
     }
@@ -91,7 +93,7 @@ class OrderTest {
         @MethodSource(ORDER_ITEMS_GENERATOR_PATH + "#createSuccessOrder")
         @DisplayName("[Success] 주문이 성공하면 예외가 발생하지 않는다.")
         void exceedCount(List<OrderItem> orderItems) {
-            Assertions.assertThatCode(() -> new Order(orderItems))
+            Assertions.assertThatCode(() -> new Order(orderItems, 29))
                     .doesNotThrowAnyException();
         }
     }
@@ -106,7 +108,7 @@ class OrderTest {
                 "초코케이크:17:255000", "아이스크림:13:65000"}, delimiter = ':')
         @DisplayName("주문 메뉴 단 건 가격 테스트")
         void onePriceTest(String menuName, int count, int expectedPrice) {
-            Order order = new Order(List.of(new OrderItem(menuName, count)));
+            Order order = new Order(List.of(new OrderItem(menuName, count)), 18);
             Assertions.assertThat(order.getTotalOrderPrice()).isEqualTo(expectedPrice);
         }
 
@@ -114,9 +116,23 @@ class OrderTest {
         @MethodSource(ORDER_ITEMS_GENERATOR_PATH + "#createSuccessOrder")
         @DisplayName("주문 메뉴 총 가격 테스트")
         void totalPriceTest(List<OrderItem> orderItem) {
-            Order order = new Order(orderItem);
+            Order order = new Order(orderItem, 12);
             int expectedPrice = 496500;
             Assertions.assertThat(order.getTotalOrderPrice()).isEqualTo(expectedPrice);
+        }
+
+        @Test
+        void temp() {
+            // given
+            List<OrderItem> orderItems = List.of(new OrderItem("양송이수프", 1),
+                    new OrderItem("티본스테이크", 1),
+                    new OrderItem("초코케이크", 11), new OrderItem("아이스크림", 5),
+                    new OrderItem("제로콜라", 1));
+            Order order = new Order(orderItems, 3);
+            // when
+            int menuTypeCount = order.getSpecificMenuOrderCount(MenuType.DESSERT);
+            // then
+            Assertions.assertThat(menuTypeCount).isEqualTo(16);
         }
     }
 }
