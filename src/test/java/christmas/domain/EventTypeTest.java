92cbb3e93dd7751c4f6d5b59d47b380557ea5cb2
june-777 <wlwhswnsrl96@gmail.com>
2023-11-14 21:48:39 +0900

    test(EventTypeTest): 크리스마스 디데이 할인 금액 계산 기능 테스트

--- src/test/java/christmas/domain/EventTypeTest.java
+++ src/test/java/christmas/domain/EventTypeTest.java
@@ -1,12 +1,54 @@
 package christmas.domain;
 
+import christmas.domain.order.Order;
+import christmas.domain.order.OrderItem;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class EventTypeTest {
+
+    @Nested
+    @DisplayName("크리스마스 할인 이벤트를 적용하려는데")
+    class ChristmasDiscountTest {
+
+        @DisplayName("적용 가능한 날짜는 날짜만큼 할인된다.")
+        @ParameterizedTest
+        @CsvSource(value = {"1:-1000", "2:-1100", "3:-1200", "4:-1300", "5:-1400", "6:-1500", "7:-1600", "8:-1700",
+                "9:-1800", "10:-1900", "11:-2000", "12:-2100", "13:-2200", "14:-2300", "15:-2400", "16:-2500",
+                "17:-2600", "18:-2700", "19:-2800", "20:-2900", "21:-3000", "22:-3100", "23:-3200", "24:-3300",
+                "25:-3400", "26:0", "27:0", "28:0", "29:0", "30:0", "31:0"}, delimiter = ':')
+        void christmasDay(int day, int expectedDiscountAmount) {
+            String menuForm = "양송이수프-1,바비큐립-2,초코케이크-3,샴페인-4";
+            Order order = createOrder(day,
+                    new OrderItem("양송이수프", 1), new OrderItem("바비큐립", 2),
+                    new OrderItem("초코케이크", 3), new OrderItem("샴페인", 4));
+            EventType eventType = EventType.CHRISTMAS;
+            int resultDiscountAmount = eventType.calculateDiscountAmount(order.getDay(), order.getDay());
+            Assertions.assertThat(resultDiscountAmount).isEqualTo(expectedDiscountAmount);
+        }
+
+        @DisplayName("적용 불가한 날짜는 0원이 할인된다.")
+        @ParameterizedTest
+        @CsvSource(value = {"26:0", "27:0", "28:0", "29:0", "30:0", "31:0"}, delimiter = ':')
+        void notChristmasDay(int day, int expectedDiscountAmount) {
+            String menuForm = "양송이수프-1,바비큐립-2,초코케이크-3,샴페인-4";
+            Order order = createOrder(day,
+                    new OrderItem("양송이수프", 1), new OrderItem("바비큐립", 2),
+                    new OrderItem("초코케이크", 3), new OrderItem("샴페인", 4));
+            EventType eventType = EventType.CHRISTMAS;
+            int resultDiscountAmount = eventType.calculateDiscountAmount(order.getDay(), order.getDay());
+            Assertions.assertThat(resultDiscountAmount).isEqualTo(expectedDiscountAmount);
+        }
+
+        private static Order createOrder(int day, OrderItem... orderItems) {
+            return new Order(List.of(orderItems), day);
+        }
+    }
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 8, 9, 15, 16, 22, 23, 29, 30})
