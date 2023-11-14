3500c6ea0918724808cf92bba3253283c3c62e86
june-777 <wlwhswnsrl96@gmail.com>
2023-11-15 08:07:43 +0900

    refactor(DiscountInformation): 패키지 위치를 domain에서 domain/event로 이동

--- src/main/java/christmas/domain/event/DiscountInformation.java
+++ src/main/java/christmas/domain/event/DiscountInformation.java
@@ -1,6 +1,11 @@
-package christmas.domain;
+package christmas.domain.event;
 
+import christmas.domain.EventType;
+import java.util.Collections;
 import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class DiscountInformation {
     private final EnumMap<EventType, Integer> eachEventDiscountAmounts;
@@ -22,5 +27,17 @@ public class DiscountInformation {
             return eachEventDiscountAmounts.get(eventType);
         }
         return 0;
+    }
+
+    public boolean isExistSpecificEventDiscount(EventType eventType) {
+        return getDiscountAmount(eventType) != 0;
+    }
+
+    public Map<String, Integer> getEachEventDiscountAmounts() {
+        Map<String, Integer> result = new HashMap<>();
+        for (Entry<EventType, Integer> eachEventDiscountAmount : eachEventDiscountAmounts.entrySet()) {
+            result.put(eachEventDiscountAmount.getKey().getEventName(), eachEventDiscountAmount.getValue());
+        }
+        return Collections.unmodifiableMap(result);
     }
 }
