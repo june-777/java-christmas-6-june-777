1152d8b3a5e1b58019fbce5fccee250efe2568c1
june-777 <wlwhswnsrl96@gmail.com>
2023-11-14 19:55:05 +0900

    feat(EventDayCalculator): 주말 할인 이벤트 날짜를 계산하는 기능

--- src/main/java/christmas/service/event/EventDayCalculator.java
+++ src/main/java/christmas/service/event/EventDayCalculator.java
@@ -8,10 +8,26 @@ public class EventDayCalculator {
     private static final int CHRISTMAS_EVENT_START = 1;
     private static final int CHRISTMAS_EVENT_END = 25;
 
+    private static final int FIRST_WEEK_FRIDAY = 1;
+    private static final int FIRST_WEEK_SATURDAY = 2;
+    private static final int DECEMBER_LAST_DAY = 31;
+    private static final int DAY_OF_WEEK = 7;
+
+
     public static List<Integer> christmasDay() {
         List<Integer> days = new ArrayList<>();
         for (int day = CHRISTMAS_EVENT_START; day <= CHRISTMAS_EVENT_END; day++) {
             days.add(day);
+        }
+        return days;
+    }
+
+    public static List<Integer> weekEnd() {
+        List<Integer> days = new ArrayList<>();
+        for (int friday = FIRST_WEEK_FRIDAY, saturday = FIRST_WEEK_SATURDAY; saturday <= DECEMBER_LAST_DAY;
+             friday += DAY_OF_WEEK, saturday += DAY_OF_WEEK) {
+            days.add(friday);
+            days.add(saturday);
         }
         return days;
     }
