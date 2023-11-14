9f0bf99f7a87cda1086696dc666e95ba5560a472
june-777 <wlwhswnsrl96@gmail.com>
2023-11-15 07:37:26 +0900

    test(FreeGiftEventServiceTest): 증정 상품 이벤트를 적용하는 기능 테스트

---
+++ src/test/java/christmas/service/event/gift/FreeGiftEventServiceTest.java
@@ -0,0 +1,33 @@
+package christmas.service.event.gift;
+
+import christmas.domain.Menu;
+import christmas.domain.event.FreeGift;
+import java.util.Optional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class FreeGiftEventServiceTest {
+
+    FreeGiftEventService freeGiftEventService = new FreeGiftEventService();
+
+    @ParameterizedTest
+    @ValueSource(ints = {119999})
+    @DisplayName("총주문 금액이 12만원 미만이면 증정 상품이 없다.")
+    void notExistFreeGift(int totalOrderPrice) {
+        Optional<FreeGift> freeGift = freeGiftEventService.applyGiftEvent(totalOrderPrice);
+        Assertions.assertThat(freeGift).isEmpty();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {120000, 131313, 123456})
+    @DisplayName("총주문 금액이 12만원 이상이면 증정 상품이 있다.")
+    void existFreeGift(int totalOrderPrice) {
+        Optional<FreeGift> freeGift = freeGiftEventService.applyGiftEvent(totalOrderPrice);
+        Assertions.assertThat(freeGift).isPresent();
+        Assertions.assertThat(freeGift.get().getGiftName()).isEqualTo(Menu.CHAMPAGNE.getName());
+        Assertions.assertThat(freeGift.get().getCount()).isEqualTo(1);
+        Assertions.assertThat(freeGift.get().calculateBenefitPrice()).isEqualTo(Menu.CHAMPAGNE.getPrice() * -1);
+    }
+}
