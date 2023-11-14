66340588ec56456da21730fd9f48a055dd377fda
june-777 <wlwhswnsrl96@gmail.com>
2023-11-14 19:59:44 +0900

    feat(EventType): 각 이벤트 유형마다 가능 날짜를 멤버로 갖도록 구현

--- src/main/java/christmas/domain/EventType.java
+++ src/main/java/christmas/domain/EventType.java
@@ -1,8 +1,21 @@
 package christmas.domain;
 
+import christmas.service.event.EventDayCalculator;
+import java.util.List;
+
 public enum EventType {
-    CHRISTMAS,
-    WEEKDAY,
-    WEEKEND,
-    SPECIAL
+    CHRISTMAS(EventDayCalculator.christmasDay()),
+    WEEKDAY(EventDayCalculator.weekDay()),
+    WEEKEND(EventDayCalculator.weekEnd()),
+    SPECIAL(EventDayCalculator.specialDay());
+
+    private final List<Integer> possibleDays;
+
+    EventType(List<Integer> possibleDays) {
+        this.possibleDays = possibleDays;
+    }
+
+    public List<Integer> getPossibleDays() {
+        return possibleDays;
+    }
 }
