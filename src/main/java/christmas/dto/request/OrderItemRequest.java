ee6400e22cc1e7997bf42dec4f58e21c27419d69
june-777 <wlwhswnsrl96@gmail.com>
2023-11-15 08:02:38 +0900

    refactor(XXXRequest): 패키지 위치를 dto에서 dto/request로 이동

--- src/main/java/christmas/dto/request/OrderItemRequest.java
+++ src/main/java/christmas/dto/request/OrderItemRequest.java
@@ -1,4 +1,4 @@
-package christmas.dto;
+package christmas.dto.request;
 
 public class OrderItemRequest {
     private final String menuName;
--- src/main/java/christmas/dto/request/OrderRequest.java
+++ src/main/java/christmas/dto/request/OrderRequest.java
@@ -1,4 +1,4 @@
-package christmas.dto;
+package christmas.dto.request;
 
 import java.util.List;
 
