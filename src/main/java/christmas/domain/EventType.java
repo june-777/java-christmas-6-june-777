fcf5339bbacbfde33913ee7e1af9b83aa2b724de
june-777 <wlwhswnsrl96@gmail.com>
2023-11-15 08:06:14 +0900

    feat(EventType): 이벤트 유형 이름 필드 추가, getter 구현

--- src/main/java/christmas/domain/EventType.java
+++ src/main/java/christmas/domain/EventType.java
@@ -6,27 +6,29 @@ import java.util.List;
 import java.util.function.Function;
 
 public enum EventType {
-    CHRISTMAS(EventDayCalculator.christmasDay(), orderDay -> {
+    CHRISTMAS("크리스마스 디데이 할인", EventDayCalculator.christmasDay(), orderDay -> {
         final int basicDiscountAmount = 1000;
         final int discountAmountUnit = 100;
         return (basicDiscountAmount + discountAmountUnit * (orderDay - 1)) * -1;
     }),
-    WEEKDAY(EventDayCalculator.weekDay(), dessertCount -> {
+    WEEKDAY("평일 할인", EventDayCalculator.weekDay(), dessertCount -> {
         final int discountAmountUnit = 2023;
         return (discountAmountUnit * dessertCount) * -1;
     }),
-    WEEKEND(EventDayCalculator.weekEnd(), mainCourseCount -> {
+    WEEKEND("주말 할인", EventDayCalculator.weekEnd(), mainCourseCount -> {
         final int discountAmountUnit = 2023;
         return (discountAmountUnit * mainCourseCount) * -1;
     }),
-    SPECIAL(EventDayCalculator.specialDay(), anything -> {
+    SPECIAL("특별 할인", EventDayCalculator.specialDay(), anything -> {
         return -1000;
     });
 
+    private final String eventName;
     private final List<Integer> possibleDays;
     private final Function<Integer, Integer> calculator;
 
-    EventType(List<Integer> possibleDays, Function<Integer, Integer> calculator) {
+    EventType(String eventName, List<Integer> possibleDays, Function<Integer, Integer> calculator) {
+        this.eventName = eventName;
         this.possibleDays = possibleDays;
         this.calculator = calculator;
     }
@@ -50,5 +52,9 @@ public enum EventType {
 
     public List<Integer> getPossibleDays() {
         return possibleDays;
+    }
+
+    public String getEventName() {
+        return eventName;
     }
 }
