66493c56000ad2d4ee4307254ccb3453a530a28d
june-777 <wlwhswnsrl96@gmail.com>
2023-11-15 22:31:52 +0900

    feat(OutputView): 주문 메뉴를 출력하는 기능

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,5 +1,7 @@
 package christmas.view;
 
+import christmas.dto.response.OrderItemResponse;
+import christmas.dto.response.OrderResponse;
 import java.text.DecimalFormat;
 
 public class OutputView {
@@ -8,11 +10,22 @@ public class OutputView {
     private static final String START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String PREVIEW_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
 
+    private static final String ORDER_MENU_MESSAGE = "\n<주문 메뉴>";
+    private static final String ORDER_MENU_RESULT_FORM = "%s %d개\n";
+
+
     public void printStartMessage() {
         System.out.println(START_MESSAGE);
     }
 
     public void printPreviewEventMessage(int date) {
         System.out.printf(PREVIEW_MESSAGE, date);
+    }
+
+    public void printOrderMenu(OrderResponse orderResponse) {
+        System.out.println(ORDER_MENU_MESSAGE);
+        for (OrderItemResponse orderItemResponse : orderResponse.getOrderItemResponses()) {
+            System.out.printf(ORDER_MENU_RESULT_FORM, orderItemResponse.getMenuName(), orderItemResponse.getCount());
+        }
     }
 }
