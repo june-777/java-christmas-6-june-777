44d97b4f98dd43ca5a42e66b4d633982da51be4c
june-777 <wlwhswnsrl96@gmail.com>
2023-11-15 22:30:55 +0900

    feat(OutputView): 혜택 미리보기 안내 메시지를 출력하는 기능

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -6,8 +6,13 @@ public class OutputView {
 
     private static final DecimalFormat PRICE_FORMAT = new DecimalFormat("###,###원\n");
     private static final String START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    private static final String PREVIEW_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
 
     public void printStartMessage() {
         System.out.println(START_MESSAGE);
+    }
+
+    public void printPreviewEventMessage(int date) {
+        System.out.printf(PREVIEW_MESSAGE, date);
     }
 }
