bbbaa21f31229e6f0713f2722ec0bc369c84e785
june-777 <wlwhswnsrl96@gmail.com>
2023-11-15 21:43:41 +0900

    feat(DateInputValidator): 숫자를 검증하는 기능

--- src/main/java/christmas/view/validator/DateInputValidator.java
+++ src/main/java/christmas/view/validator/DateInputValidator.java
@@ -1,11 +1,16 @@
 package christmas.view.validator;
 
 import christmas.view.exception.InputDateBlankException;
+import christmas.view.exception.InputDateNotNumericException;
+import java.util.regex.Pattern;
 
 public class DateInputValidator {
 
+    private static final Pattern NUMERIC_REGEX = Pattern.compile("[1-9]\\d*");
+
     public static void validate(String target) {
         validateBlank(target);
+        validateNumeric(target);
     }
 
     private static void validateBlank(String target) {
@@ -13,4 +18,11 @@ public class DateInputValidator {
             throw new InputDateBlankException();
         }
     }
+
+    private static void validateNumeric(String target) {
+        if (!NUMERIC_REGEX.matcher(target).matches()) {
+            throw new InputDateNotNumericException();
+        }
+    }
+
 }
