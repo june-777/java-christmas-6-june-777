b979939332a78d05e6c550d3023b4f04fc773c9c
june-777 <wlwhswnsrl96@gmail.com>
2023-11-15 22:38:53 +0900

    feat(Application): 프로그램 시작 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,12 @@
 package christmas;
 
+import christmas.configuration.ApplicationConfiguration;
+import christmas.controller.FrontController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ApplicationConfiguration applicationConfiguration = new ApplicationConfiguration();
+        FrontController frontController = applicationConfiguration.frontController();
+        frontController.run();
     }
 }
